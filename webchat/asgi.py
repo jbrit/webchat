7ac0ac5f279c95ed46cbef8e58583d56c8d60b6f
jbrit <pro.ajibolaojo@gmail.com>
2021-04-21 14:12:01 +0100

    removed daphne settings arg

--- webchat/asgi.py
+++ webchat/asgi.py
@@ -1,12 +1,11 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import rooms.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webchat.settings')
-
+django.setup()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": 
